cb3e8fc0851686ccb0b6ca429f2c81f577977243
centmaster <861676131@qq.com>
2017-05-04 00:22:42 +0800

    add

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,9 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    <h1 v-text="msg"></h1>
+    <input /><button v-on:click="additem">add</button>
+    <ul v-for="item in items">
+      <li v-bind:class="{done:item.isDone}"  >{{item.item}}</li>
     </ul>
   </div>
 </template>
@@ -25,14 +13,34 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome  to Your Vue.js App'
+      msg: 'this is a todoList',
+      items:[
+        {
+            item:'wash cloth',
+            isDone:false
+        },
+        {
+          item:'play football',
+          isDone:true
+        }
+      ]
+
     }
+  },
+  methods(){
+        additem:function () {
+          alert('hha');
+        }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+  .done{
+  text-decoration: underline;
+  }
 h1, h2 {
   font-weight: normal;
 }
